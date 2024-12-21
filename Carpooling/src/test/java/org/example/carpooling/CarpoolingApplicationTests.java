445985d67bff3ac49cc95645853aeff1d5b387ee
Vladislav Dimitrov <vladislav.d.dimit@gmail.com>
2024-12-22 00:44:49 +0200

    Initial commit

---
+++ Carpooling/src/test/java/org/example/carpooling/CarpoolingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.carpooling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarpoolingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
