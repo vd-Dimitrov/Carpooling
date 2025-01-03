133edfd60dbe2d7319604bf3f85f02d86a990072
Vladislav Dimitrov <vladislav.d.dimit@gmail.com>
2025-01-03 23:52:01 +0200

    Update User.java

--- Carpooling/src/main/java/org/example/carpooling/models/User.java
+++ Carpooling/src/main/java/org/example/carpooling/models/User.java
@@ -59,4 +59,28 @@ public class User {
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
 }
