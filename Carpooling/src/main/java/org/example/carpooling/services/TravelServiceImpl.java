a0fc05a0215e0f8d65de9f10acc9aff29d8962b6
Vladislav Dimitrov <vladislav.d.dimit@gmail.com>
2025-01-07 17:52:46 +0200

    Added functionalities to TravelService

---
+++ Carpooling/src/main/java/org/example/carpooling/services/TravelServiceImpl.java
@@ -0,0 +1,54 @@
+package org.example.carpooling.services;
+
+import org.example.carpooling.exceptions.AuthorizationException;
+import org.example.carpooling.exceptions.EntityNotFoundException;
+import org.example.carpooling.models.Travel;
+import org.example.carpooling.models.User;
+import org.example.carpooling.repositories.TravelRepository;
+import org.example.carpooling.services.interfaces.TravelService;
+
+import java.util.List;
+
+public class TravelServiceImpl implements TravelService {
+    public static final String MODIFY_ERROR_MESSAGE = "Only author can make changes to the travel information!";
+    private final TravelRepository travelRepository;
+
+    public TravelServiceImpl(TravelRepository travelRepository) {
+        this.travelRepository = travelRepository;
+    }
+
+    @Override
+    public Travel createTravel(Travel travel) {
+        return travelRepository.save(travel);
+    }
+
+    @Override
+    public Travel getById(int travelId) {
+        return travelRepository.findTravelByTravelId(travelId)
+                .orElseThrow( () -> new EntityNotFoundException("Travel", travelId));
+    }
+
+    @Override
+    public List<Travel> getByDriver(int driveId) {
+        return travelRepository.findAllByDriverUserId(driveId)
+                .orElseThrow( () -> new EntityNotFoundException("Driver", driveId));
+    }
+
+    @Override
+    public void updateTravel(Travel updatedTravel, User requestingUser) {
+        checkPermission(updatedTravel, requestingUser);
+        travelRepository.save(updatedTravel);
+    }
+
+    @Override
+    public void deleteTravel(Travel travelToDelete, User requestingUser) {
+        checkPermission(travelToDelete, requestingUser);
+        travelRepository.delete(travelToDelete);
+    }
+    
+    private void checkPermission(Travel updatedTravel, User requestingUser){
+        if (requestingUser.getUserId()!=updatedTravel.getDriver().getUserId()){
+            throw new AuthorizationException(MODIFY_ERROR_MESSAGE);
+        }
+    }
+}
