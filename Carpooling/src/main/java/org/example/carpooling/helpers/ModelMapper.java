7af3589a9eeb4f7f3711e0cabf5686fcf0fe06ae
Vladislav Dimitrov <vladislav.d.dimit@gmail.com>
2025-01-11 19:31:43 +0200

    Added; More functionalities to ModelMapper

--- Carpooling/src/main/java/org/example/carpooling/helpers/ModelMapper.java
+++ Carpooling/src/main/java/org/example/carpooling/helpers/ModelMapper.java
@@ -1,21 +1,49 @@
 package org.example.carpooling.helpers;
 
 import org.example.carpooling.models.User;
-import org.example.carpooling.models.dto.UserDto;
+import org.example.carpooling.models.dto.UserDtoIn;
+import org.example.carpooling.models.dto.UserDtoOut;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class ModelMapper {
-    public User fromUserDto(UserDto userDto) {
+    public User fromUserDto(UserDtoIn userDtoIn) {
         User user = new User();
 
-        user.setUsername(userDto.getUsername());
-        user.setPassword(userDto.getPassword());
-        user.setFirstName(userDto.getFirstName());
-        user.setLastName(userDto.getLastName());
-        user.setEmail(userDto.getEmail());
-        user.setPhoneNumber(userDto.getPhoneNumber());
+        user.setUsername(userDtoIn.getUsername());
+        user.setPassword(userDtoIn.getPassword());
+        user.setFirstName(userDtoIn.getFirstName());
+        user.setLastName(userDtoIn.getLastName());
+        user.setEmail(userDtoIn.getEmail());
+        user.setPhoneNumber(userDtoIn.getPhoneNumber());
 
         return user;
+    }
+
+
+    public UserDtoOut fromUserToUserDto(User user){
+        UserDtoOut userDtoOut = new UserDtoOut();
+
+        userDtoOut.setUsername(user.getUsername());
+        userDtoOut.setFirstName(user.getFirstName());
+        userDtoOut.setLastName(user.getLastName());
+        userDtoOut.setEmail(user.getEmail());
+        userDtoOut.setPhoneNumber(user.getPhoneNumber());
+
+        return userDtoOut;
+    }
+
+    public List<UserDtoOut> fromListUsersToListUserDto(List<User> users){
+        if (users == null){
+            return new ArrayList<>();
+        }
+
+        return users.stream()
+                .map(this::fromUserToUserDto)
+                .collect(Collectors.toList());
     }
 }
