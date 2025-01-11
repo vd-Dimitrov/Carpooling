9a074b20145b6ed3c85441a99e7a2e5242e967b2
Vladislav Dimitrov <vladislav.d.dimit@gmail.com>
2025-01-11 19:31:23 +0200

    Renamed: UserDto to UserDtoIn

--- Carpooling/src/main/java/org/example/carpooling/models/dto/UserDtoIn.java
+++ Carpooling/src/main/java/org/example/carpooling/models/dto/UserDtoIn.java
@@ -3,7 +3,7 @@ package org.example.carpooling.models.dto;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-public class UserDto {
+public class UserDtoIn {
 
     @NotNull(message = "Username cannot be empty")
     @Size(min = 2, max = 20, message = "Username should be between 2 and 20 characters")
