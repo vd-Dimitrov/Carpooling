41d4ed54d75dd595f6afe5821835b34f5bbc38bc
Vladislav Dimitrov <vladislav.d.dimit@gmail.com>
2025-01-13 16:15:53 +0200

    Completed: UnitTests for UserServiceImpl

--- Carpooling/src/test/java/org/example/carpooling/Helpers.java
+++ Carpooling/src/test/java/org/example/carpooling/Helpers.java
@@ -15,4 +15,17 @@ public class Helpers {
 
         return mockUser;
     }
+
+    public static User createMockUserInvalidEmail(){
+        User mockUser = new User();
+        mockUser.setUserId(1);
+        mockUser.setUsername("MockUsername");
+        mockUser.setPassword("MockPassword");
+        mockUser.setFirstName("MockFirstName");
+        mockUser.setLastName("MockLastName");
+        mockUser.setPhoneNumber("0123456789");
+        mockUser.setEmail("InvalidMockEmail");
+
+        return mockUser;
+    }
 }
--- Carpooling/src/test/java/org/example/carpooling/servicesTests/UserServiceImplTests.java
+++ Carpooling/src/test/java/org/example/carpooling/servicesTests/UserServiceImplTests.java
@@ -1,5 +1,7 @@
 package org.example.carpooling.servicesTests;
 
+import org.example.carpooling.exceptions.AuthorizationException;
+import org.example.carpooling.exceptions.EntityDuplicateException;
 import org.example.carpooling.exceptions.EntityNotFoundException;
 import org.example.carpooling.models.User;
 import org.example.carpooling.repositories.UserRepository;
@@ -12,6 +14,8 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.example.carpooling.Helpers.*;
@@ -25,22 +29,6 @@ public class UserServiceImplTests {
     @BeforeEach
     public void setUp(){
         MockitoAnnotations.openMocks(this);
-    }
-
-    @Test
-    public void getByUsername_Should_ReturnUser_When_UsernameExists(){
-        // Arrange
-        User mockUser = createMockUser();
-
-
-        Mockito.when(mockUserRepository.findUserByUsername(Mockito.anyString()))
-                .thenReturn(Optional.of(mockUser));
-
-        // Act
-        User result = mockUserService.getByUsername(Mockito.anyString());
-
-        // Assert
-        Assertions.assertEquals(mockUser, result);
     }
 
     @Test
@@ -59,5 +47,149 @@ public class UserServiceImplTests {
                 .save(mockUser);
     }
 
+    @Test
+    public void createUser_Should_Throw_When_UserWithSameUsernameExist(){
+        // Arrange
+        User mockUser = createMockUser();
+
+        Mockito.when(mockUserRepository.findUserByUsername(Mockito.anyString()))
+                .thenReturn(Optional.of(mockUser));
+
+        // Act, Assert
+        Assertions.assertThrows(EntityDuplicateException.class,
+                () -> mockUserService.createUser(mockUser));
+    }
+
+    @Test
+    public void createUser_Should_Throw_When_UserWithSameEmailExists(){
+        // Arrange
+        User mockUser = createMockUser();
+
+        Mockito.when(mockUserRepository.findUserByEmail(Mockito.anyString()))
+                .thenReturn(Optional.of(mockUser));
+
+        // Act, Assert
+        Assertions.assertThrows(EntityDuplicateException.class,
+                () -> mockUserService.createUser(mockUser));
+
+    }
+
+    @Test
+    public void createUser_Should_Throw_When_UserWithSamePhoneNumberExists(){
+        // Arrange
+        User mockUser = createMockUser();
+
+        Mockito.when(mockUserRepository.findUserByPhoneNumber(Mockito.anyString()))
+                .thenReturn(Optional.of(mockUser));
+
+        // Act, Assert
+        Assertions.assertThrows(EntityDuplicateException.class,
+                () -> mockUserService.createUser(mockUser));
+
+    }
+
+    @Test
+    public void createUser_Should_Throw_When_EmailIsInvalid(){
+        // Arrange
+        User mockUser = createMockUserInvalidEmail();
+
+        // Act, Assert
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> mockUserService.createUser(mockUser));
+    }
+    @Test
+    public void getByUsername_Should_ReturnUser_When_UsernameExists(){
+        // Arrange
+        User mockUser = createMockUser();
+
+
+        Mockito.when(mockUserRepository.findUserByUsername(Mockito.anyString()))
+                .thenReturn(Optional.of(mockUser));
+
+        // Act
+        User result = mockUserService.getByUsername(Mockito.anyString());
+
+        // Assert
+        Assertions.assertEquals(mockUser, result);
+    }
+
+    @Test
+    public void getAllUsers_Should_CallRepository(){
+        // Arrange
+        List<User> mockUserList = new ArrayList<>();
+
+        Mockito.when(mockUserRepository.findAll())
+                .thenReturn(mockUserList);
+
+        //Act
+        List<User> result = mockUserService.getAllUsers();
+
+        // Assert
+        Assertions.assertEquals(mockUserList, result);
+    }
+    @Test
+    public void getById_Should_ReturnUser_When_IdExists(){
+        // Arrange
+        User mockUser = createMockUser();
+
+        Mockito.when(mockUserRepository.findUserByUserId(Mockito.anyInt()))
+                .thenReturn(Optional.of(mockUser));
+
+        // Act
+        User result = mockUserService.getById(Mockito.anyInt());
+
+        // Assert
+        Assertions.assertEquals(mockUser, result);
+    }
+
+    @Test
+    public void updateUser_Should_CallRepository_When_UserIsOwner(){
+        // Arrange
+        User mockUser = createMockUser();
+
+        // Act
+        mockUserService.updateUser(mockUser, mockUser);
+
+        // Assert
+        Mockito.verify(mockUserRepository, Mockito.times(1))
+                .save(mockUser);
+    }
+
+    @Test
+    public void updateUser_Should_Throw_When_UserIsNotOwner(){
+        // Arrange
+        User mockUser = createMockUser();
+        User mockInvalidUser = createMockUser();
+        mockInvalidUser.setUserId(2);
+
+        // Act, Assert
+        Assertions.assertThrows(AuthorizationException.class,
+                () -> mockUserService.updateUser(mockUser, mockInvalidUser));
+    }
+
+    @Test
+    public void deleteUser_Should_CallRepository_When_UserIsOwner(){
+        // Arrange
+        User mockUser = createMockUser();
+
+        // Act
+        mockUserService.deleteUser(mockUser, mockUser);
+
+        // Assert
+        Mockito.verify(mockUserRepository, Mockito.times(1))
+                .delete(mockUser);
+    }
+
+    @Test
+    public void deleteUser_Should_Throw_When_UserIsNotOwner(){
+        // Arrange
+        User mockUser = createMockUser();
+        User mockInvalidUser = createMockUser();
+        mockInvalidUser.setUserId(2);
+
+        // Act, Assert
+        Assertions.assertThrows(AuthorizationException.class,
+                () -> mockUserService.deleteUser(mockUser, mockInvalidUser));
+    }
 
 }
