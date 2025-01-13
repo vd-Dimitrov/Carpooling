b35ef0dbb12494ed6076c2d9397f22bb8dd0f9bb
Vladislav Dimitrov <vladislav.d.dimit@gmail.com>
2025-01-13 12:56:02 +0200

    Update User model: equals and hash

--- Carpooling/src/main/java/org/example/carpooling/models/User.java
+++ Carpooling/src/main/java/org/example/carpooling/models/User.java
@@ -2,6 +2,8 @@ package org.example.carpooling.models;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
+
 @Entity
 @Table(name = "users")
 public class User {
@@ -93,5 +95,18 @@ public class User {
 
     public void setAdmin(boolean admin) {
         isAdmin = admin;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return userId == user.userId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(userId);
     }
 }
