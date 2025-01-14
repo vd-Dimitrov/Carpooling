2322198b82724310ab5b14b04a15f1d90fbd91fa
Vladislav Dimitrov <vladislav.d.dimit@gmail.com>
2025-01-14 17:44:05 +0200

    Fix ups

--- Carpooling/src/main/java/org/example/carpooling/controllers/UserRestController.java
+++ Carpooling/src/main/java/org/example/carpooling/controllers/UserRestController.java
@@ -19,7 +19,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api/users")
 public class UserRestController {
     private final UserService userService;
     private final ModelMapper modelMapper;
