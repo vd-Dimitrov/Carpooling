fcb4c07cdb3c4be00db7bf4611bf16e30a2e9797
Vladislav Dimitrov <vladislav.d.dimit@gmail.com>
2025-01-15 13:13:10 +0200

    adjustments

--- Carpooling/src/main/java/org/example/carpooling/controllers/UserRestController.java
+++ Carpooling/src/main/java/org/example/carpooling/controllers/UserRestController.java
@@ -13,6 +13,7 @@ import org.example.carpooling.models.dto.UserDtoUpdate;
 import org.example.carpooling.services.interfaces.UserService;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
