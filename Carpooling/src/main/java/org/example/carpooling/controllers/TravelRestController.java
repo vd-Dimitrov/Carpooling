d3dbdd93286efcaca39036db10fb3b40cc3f42b9
Vladislav Dimitrov <vladislav.d.dimit@gmail.com>
2025-01-24 18:53:33 +0200

    Update TravelRestController

--- Carpooling/src/main/java/org/example/carpooling/controllers/TravelRestController.java
+++ Carpooling/src/main/java/org/example/carpooling/controllers/TravelRestController.java
@@ -5,8 +5,10 @@ import org.example.carpooling.exceptions.EntityNotFoundException;
 import org.example.carpooling.helpers.AuthenticationHelper;
 import org.example.carpooling.helpers.ModelMapper;
 import org.example.carpooling.models.Travel;
+import org.example.carpooling.models.User;
 import org.example.carpooling.models.dto.TravelDtoOut;
 import org.example.carpooling.services.interfaces.TravelService;
+import org.example.carpooling.services.interfaces.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpHeaders;
@@ -18,13 +20,15 @@ import java.util.List;
 @RequestMapping("/api/travel")
 public class TravelRestController {
     private final TravelService travelService;
+    private final UserService userService;
     private final ModelMapper modelMapper;
     private final AuthenticationHelper authenticationHelper;
 
 
-    public TravelRestController(TravelService travelService, ModelMapper modelMapper,
+    public TravelRestController(TravelService travelService, UserService userService, ModelMapper modelMapper,
                                 AuthenticationHelper authenticationHelper) {
         this.travelService = travelService;
+        this.userService = userService;
         this.modelMapper = modelMapper;
         this.authenticationHelper = authenticationHelper;
     }
@@ -52,6 +56,21 @@ public class TravelRestController {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
         } catch (AuthorizationException e) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, e.getMessage());
+        }
+    }
+    
+    @GetMapping("/user/{id}")
+    public List<TravelDtoOut> getAllTravelsOfUser(@RequestHeader HttpHeaders httpHeaders,
+                                                  @PathVariable int id){
+        try {
+            authenticationHelper.tryGetUser(httpHeaders);
+            List<Travel> travels = travelService.getByDriver(id);
+
+            return modelMapper.fromListTravelsToListTravelDtoOut(travels);
+        } catch (AuthorizationException e){
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, e.getMessage());
+        } catch (EntityNotFoundException e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
         }
     }
 }
