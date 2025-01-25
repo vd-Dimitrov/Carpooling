97faf5735c6b624ecbf8824e1a8582df49d26d87
Vladislav Dimitrov <vladislav.d.dimit@gmail.com>
2025-01-25 13:30:44 +0200

    Create TravelDtoIn

---
+++ Carpooling/src/main/java/org/example/carpooling/models/dto/TravelDtoIn.java
@@ -0,0 +1,65 @@
+package org.example.carpooling.models.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class TravelDtoIn {
+    private String startingPoint;
+    private String endingPoint;
+    private String driverName;
+    private LocalDateTime departureTime;
+    private int freeSpots;
+    private String travelStatus;
+
+    public TravelDtoIn() {
+    }
+
+    public String getStartingPoint() {
+        return startingPoint;
+    }
+
+    public void setStartingPoint(String startingPoint) {
+        this.startingPoint = startingPoint;
+    }
+
+    public String getEndingPoint() {
+        return endingPoint;
+    }
+
+    public void setEndingPoint(String endingPoint) {
+        this.endingPoint = endingPoint;
+    }
+
+    public String getDriverName() {
+        return driverName;
+    }
+
+    public void setDriverName(String driverName) {
+        this.driverName = driverName;
+    }
+
+    public LocalDateTime getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(LocalDateTime departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public int getFreeSpots() {
+        return freeSpots;
+    }
+
+    public void setFreeSpots(int freeSpots) {
+        this.freeSpots = freeSpots;
+    }
+
+    public String getTravelStatus() {
+        return travelStatus;
+    }
+
+    public void setTravelStatus(String travelStatus) {
+        this.travelStatus = travelStatus;
+    }
+
+}
