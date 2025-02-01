3fe3433560b23c500afcf9df3f4b60004c9e813b
Vladislav Dimitrov <vladislav.d.dimit@gmail.com>
2025-02-01 21:13:31 +0200

    More travel tests

--- Carpooling/src/test/java/org/example/carpooling/controllers/TravelRestControllerTests.java
+++ Carpooling/src/test/java/org/example/carpooling/controllers/TravelRestControllerTests.java
@@ -1,5 +1,6 @@
 package org.example.carpooling.controllers;
 
+import org.example.carpooling.exceptions.EntityNotFoundException;
 import org.example.carpooling.helpers.AuthenticationHelper;
 import org.example.carpooling.helpers.ModelMapper;
 import org.example.carpooling.models.Travel;
@@ -44,7 +45,7 @@ public class TravelRestControllerTests {
 
     @Test
     public void getAllTravels_Should_ReturnStatusOk() throws Exception{
-        // Act
+        // Arrange
         List<Travel> mockTravelList = new ArrayList<>();
 
         Mockito.when(mockTravelService.getAllTravels()).thenReturn(mockTravelList);
@@ -61,10 +62,85 @@ public class TravelRestControllerTests {
                 .thenThrow(new ResponseStatusException(HttpStatus.UNAUTHORIZED, null));
 
         // Act, Assert
-        String body = toJson(createMockTravel());
+        String body = toJson(createMockTravelDtoOut());
         mockMvc.perform(MockMvcRequestBuilders.get("/api/travel")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(body))
                 .andExpect(MockMvcResultMatchers.status().isUnauthorized());
+    }
+
+    @Test
+    public void getTravelById_Should_ReturnStatusOk_When_UserExists() throws Exception{
+        // Arrange
+        User mockUser = createMockUser();
+
+        Mockito.when(mockUserService.getById(Mockito.anyInt()))
+                .thenReturn(mockUser);
+
+        Travel mockTravel = createMockTravel();
+        Mockito.when(mockTravelService.getById(Mockito.anyInt()))
+                .thenReturn(mockTravel);
+
+        // Act, Assert
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/travel/{id}", 1))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    public void getTravelById_Should_ReturnStatusNotFound_When_TravelDoesNotExist() throws Exception{
+        // Arrange
+        Mockito.when(mockTravelService.getById(Mockito.anyInt()))
+                .thenThrow(EntityNotFoundException.class);
+
+        // Act, Assert
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/travel/{id}", 1))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    public void getTravelById_Should_ReturnStatusInvalidAuthorization_When_InvalidAuthentication() throws  Exception{
+        Mockito.when(mockAuthenticationHelper.tryGetUser(Mockito.any()))
+                .thenThrow(new ResponseStatusException(HttpStatus.UNAUTHORIZED));
+
+        String body = toJson(createMockTravelDtoOut());
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/travel/{id}", 1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(body))
+                .andExpect(MockMvcResultMatchers.status().isUnauthorized());
+    }
+
+    @Test
+    public void getAllTravelsOfUser_Should_ReturnStatusOk() throws Exception{
+        User mockUser = createMockUser();
+
+        Mockito.when(mockAuthenticationHelper.tryGetUser(Mockito.any()))
+                .thenReturn(mockUser);
+
+        List<Travel> mockTravelList = new ArrayList<>();
+
+        Mockito.when(mockTravelService.getAllTravels())
+                .thenReturn(mockTravelList);
+
+        // Act, Assert
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/travel/user/{id}", 1))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    public void getAllTravelsOfUser_Should_ReturnStatusEntityNotFound_When_UserDoesNotExist() throws Exception{
+        Mockito.when(mockAuthenticationHelper.tryGetUser(Mockito.any()))
+                .thenThrow(EntityNotFoundException.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/travel/user/{id}", 1))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    public void createTravel_Should_ReturnStatusOk_When_CorrectRequest() throws Exception{
+        String body = toJson(createMockTravelDtoIn());
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/travel/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(body))
+                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 }
