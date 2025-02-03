21ff88f0a0ec498fd16020f771282b94799819de
Vladislav Dimitrov <vladislav.d.dimit@gmail.com>
2025-02-03 17:03:32 +0200

    Create FeedbackDtoOut

---
+++ Carpooling/src/main/java/org/example/carpooling/models/dto/FeedbackDtoOut.java
@@ -0,0 +1,35 @@
+package org.example.carpooling.models.dto;
+
+public class FeedbackDtoOut {
+    private double rating;
+    private String comment;
+    private String author;
+
+    public FeedbackDtoOut(){
+
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
