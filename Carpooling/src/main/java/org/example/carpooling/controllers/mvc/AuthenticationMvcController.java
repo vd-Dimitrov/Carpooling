14d775fbb284ba1874d998abdc688160e6adf680
Vladislav Dimitrov <vladislav.d.dimit@gmail.com>
2025-02-14 12:52:56 +0200

    Added all base functionalities to mvcAuthentication

--- Carpooling/src/main/java/org/example/carpooling/controllers/mvc/AuthenticationMvcController.java
+++ Carpooling/src/main/java/org/example/carpooling/controllers/mvc/AuthenticationMvcController.java
@@ -3,9 +3,13 @@ package org.example.carpooling.controllers.mvc;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import org.example.carpooling.exceptions.AuthorizationException;
+import org.example.carpooling.exceptions.EntityDuplicateException;
 import org.example.carpooling.helpers.AuthenticationHelper;
+import org.example.carpooling.helpers.ModelMapper;
 import org.example.carpooling.models.User;
 import org.example.carpooling.models.dto.LoginDto;
+import org.example.carpooling.models.dto.UserDtoIn;
+import org.example.carpooling.services.interfaces.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,10 +23,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/auth")
 public class AuthenticationMvcController {
     private final AuthenticationHelper authenticationHelper;
+    private final ModelMapper modelMapper;
+    private final UserService userService;
 
     @Autowired
-    public AuthenticationMvcController(AuthenticationHelper authenticationHelper) {
+    public AuthenticationMvcController(AuthenticationHelper authenticationHelper, ModelMapper modelMapper, UserService userService) {
         this.authenticationHelper = authenticationHelper;
+        this.modelMapper = modelMapper;
+        this.userService = userService;
     }
 
     @ModelAttribute("isAuthenticated")
@@ -45,12 +53,58 @@ public class AuthenticationMvcController {
        }
 
        try {
-            User user = authenticationHelper.verifyUser(login.getUsername(), login.getPassword());
+            authenticationHelper.verifyUser(login.getUsername(), login.getPassword());
             httpSession.setAttribute("currentUser", login.getUsername());
             return "redirect:/";
        } catch (AuthorizationException e){
            bindingResult.rejectValue("username", "auth_error", e.getMessage());
            return "LoginView";
        }
+    }
+
+    @GetMapping("/logout")
+    public String handleLogout(HttpSession httpSession){
+        httpSession.removeAttribute("currentUser");
+        return "redirect:/";
+    }
+
+    @GetMapping("/register")
+    public String showRegisterPage(Model model){
+        model.addAttribute("register", new UserDtoIn());
+        return "RegisterView";
+    }
+
+    @PostMapping("/register")
+    public String handleRegister(@Valid @ModelAttribute("register")UserDtoIn userDtoIn,
+                                 BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            return "RegisterView";
+        }
+
+        if (!userDtoIn.getPassword().equals(userDtoIn.getPasswordConfirm())){
+            bindingResult.rejectValue("passwordConfirm", "password_error", "Passwords should match");
+            return "RegisterView";
+        }
+
+        try{
+            User user = modelMapper.fromUserDto(userDtoIn);
+            userService.createUser(user);
+            return "redirect:/auth/login";
+        } catch (EntityDuplicateException e){
+            String rejectedValue = duplicateErrorCause(e.getMessage());
+            bindingResult.rejectValue(rejectedValue, rejectedValue+"_error", e.getMessage());
+            return "RegisterView";
+        }
+    }
+
+
+    private String duplicateErrorCause(String errorMessage){
+        String[] splitStr = errorMessage.split("\\s+");
+        return switch (splitStr[1]) {
+            case "username" -> "username";
+            case "email" -> "email";
+            case "phone" -> "phone number";
+            default -> "No error";
+        };
     }
 }
