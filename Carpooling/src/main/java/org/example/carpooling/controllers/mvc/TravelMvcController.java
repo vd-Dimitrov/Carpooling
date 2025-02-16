8205edcd9fdc8fb7b789cd0bcc2007355f4d0af5
Vladislav Dimitrov <vladislav.d.dimit@gmail.com>
2025-02-16 16:48:17 +0200

    Add paginatedSearch to mvcTravelController

--- Carpooling/src/main/java/org/example/carpooling/controllers/mvc/TravelMvcController.java
+++ Carpooling/src/main/java/org/example/carpooling/controllers/mvc/TravelMvcController.java
@@ -2,17 +2,24 @@ package org.example.carpooling.controllers.mvc;
 
 import jakarta.servlet.http.HttpSession;
 import org.example.carpooling.exceptions.AuthorizationException;
+import org.example.carpooling.exceptions.EntityNotFoundException;
 import org.example.carpooling.helpers.AuthenticationHelper;
+import org.example.carpooling.models.Travel;
+import org.example.carpooling.models.dto.TravelSearchDto;
 import org.example.carpooling.services.interfaces.TravelService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/travels")
 public class TravelMvcController {
+    public static final String LISTINGS_PER_PAGE = "8";
     private final TravelService travelService;
     private final AuthenticationHelper authenticationHelper;
 
@@ -28,13 +35,52 @@ public class TravelMvcController {
     }
 
     @GetMapping
-    public String getAllTravels(HttpSession httpSession){
+    public String showAllTravels(@RequestParam(defaultValue = "0") int page,
+                                @RequestParam(defaultValue = LISTINGS_PER_PAGE) int size,
+                                @RequestParam(defaultValue = "asc") String sortDirection,
+                                @ModelAttribute("TravelSearchDto")TravelSearchDto searchDto,
+                                Model model,
+                                HttpSession httpSession) {
         try{
             authenticationHelper.tryGetCurrentUser(httpSession);
         } catch(AuthorizationException e){
             return "redirect:/auth/login";
         }
 
-        return "travel/travel-view";
+        Sort sort = Sort.by(sortDirection.equalsIgnoreCase("asc") ? Sort.Direction.ASC : Sort.Direction.DESC);
+        PageRequest pageRequest = PageRequest.of(page, size, sort);
+
+        Page<Travel> travelPage = travelService.searchTravelsPaginated(
+                searchDto.getStartingPoint(),
+                searchDto.getEndingPoint(),
+                searchDto.getStartingPoint(),
+                searchDto.getTravelStatus(),
+                searchDto.getFreeSpots(),
+                pageRequest);
+        model.addAttribute("travelPage", travelPage.getContent());
+        model.addAttribute("travelsSize", travelPage.getTotalElements());
+        model.addAttribute("currentPage",travelPage.getNumber());
+        model.addAttribute("totalPages",travelPage.getTotalPages());
+        model.addAttribute("sortDirection", sortDirection);
+
+        return "travelsView";
+    }
+
+    @GetMapping("/{travelId}")
+    public String showSingleTravel(@PathVariable int travelId, Model model, HttpSession httpSession){
+        try {
+            authenticationHelper.tryGetCurrentUser(httpSession);
+            model.addAttribute("travel", travelService.getById(travelId));
+
+            return "travelView";
+        } catch (AuthorizationException e){
+            model.addAttribute("statusCode", HttpStatus.UNAUTHORIZED.getReasonPhrase());
+            model.addAttribute("error", e.getMessage());
+            return "ErrorView";
+        } catch (EntityNotFoundException e){
+            model.addAttribute("statusCode", HttpStatus.NOT_FOUND.getReasonPhrase());
+            model.addAttribute("error", e.getMessage());
+            return "ErrorView";
+        }
     }
 }
---
+++ Carpooling/src/main/java/org/example/carpooling/models/dto/TravelSearchDto.java
@@ -0,0 +1,52 @@
+package org.example.carpooling.models.dto;
+
+public class TravelSearchDto {
+    private String startingPoint;
+    private String endingPoint;
+    private String departureTime;
+    private String travelStatus;
+    private int freeSpots;
+
+    public TravelSearchDto() {
+    }
+
+    public String getStartingPoint() {
+        return startingPoint;
+    }
+
+    public void setStartingPoint(String startingPoint) {
+        this.startingPoint = startingPoint;
+    }
+
+    public String getEndingPoint() {
+        return endingPoint;
+    }
+
+    public void setEndingPoint(String endingPoint) {
+        this.endingPoint = endingPoint;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public String getTravelStatus() {
+        return travelStatus;
+    }
+
+    public void setTravelStatus(String travelStatus) {
+        this.travelStatus = travelStatus;
+    }
+
+    public int getFreeSpots() {
+        return freeSpots;
+    }
+
+    public void setFreeSpots(int freeSpots) {
+        this.freeSpots = freeSpots;
+    }
+}
